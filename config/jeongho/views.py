0400c4ac80d0e54c6b7f164677b3157358d4cca0
JeongH0 <alswjdgh5095@gmail.com>
2021-05-07 23:04:39 +0900

    connect html and make views.py

--- config/jeongho/views.py
+++ config/jeongho/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
 def jeongho_home(request):
-    return render(request, template_name = 'jeongho/jeongho_home.html')
+    return render(request, template_name='jeongho/jeongho_home.html')
